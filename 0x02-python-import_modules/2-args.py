08b1dad133f25b0e12ca06460198e62afe2a13fa
truong21 <543@holbertonschool.com>
2018-11-29 18:35:26 +0000

    added specifiers in if else statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,11 @@
 if __name__ == "__main__":
     import sys
     arg = len(sys.argv)
-    if arg == 0:
-        print("0 arguments.")
+    if arg == 1:
+        print("{:d} argument".format(arg - 1))
     elif arg == 2:
-        print("1 argument:")
-        print("{:s}".format(sys.argv[1]))
+        print("{:d} argument:".format(arg - 1))
     else:
         print("{:d} arguments:".format(arg - 1))
-        for i in range(1, arg):
+    for i in range(1, arg):
             print("{:d}: {:s}".format(i, sys.argv[i]))
