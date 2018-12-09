e18ad83eab65b9d06ba5411cda47251aa117e379
truong21 <543@holbertonschool.com>
2018-12-09 02:21:11 +0000

    fix edge case for empty list and multiple node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,6 +19,12 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	curr = prev = *head;
 	curr_num = prev_num = curr->n;
+	if (curr == NULL || curr_num > number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	} 
 	while (curr != NULL)
 	{
 		if (curr->next == NULL || (number <= curr_num && number >= prev_num))
