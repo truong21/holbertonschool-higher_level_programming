e9371a7f7fea85ee4f0e7e6ae9d5ae3e191d9757
truong21 <543@holbertonschool.com>
2018-12-14 07:25:00 +0000

    roman numeral to number

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    sum = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in range(len(roman_string)):
+        value = roman[roman_string[i]]
+        if i + 1 < len(roman_string) and roman[roman_string[i+1]] > value:
+            sum -= value
+        else:
+            sum += value
+    return sum
