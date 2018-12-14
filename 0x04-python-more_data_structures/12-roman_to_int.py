1c3dbed1fa6a99525133eb1be6861d92f199b952
truong21 <543@holbertonschool.com>
2018-12-14 08:14:20 +0000

    check edge case for empty string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == "" or len(roman_string) == 0 or roman_string is None:
+    if len(roman_string) == 0 or roman_string is None:
         return 0
     sum = 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
