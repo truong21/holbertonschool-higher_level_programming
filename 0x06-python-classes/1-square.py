8a4f708ddcdf3facc9ba682e2bcad78c129970ca
truong21 <543@holbertonschool.com>
2019-01-04 03:50:34 +0000

    creates a class square that defines the square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class Square: contains size
+"""
+
+
+class Square:
+    """Create an square with size"""
+    def __init__(self, size):
+        """ initialize private size variable"""
+        self.__size = size
