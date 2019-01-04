e54bc91b22112dcadfb5da0ec5191f4773b32755
truong21 <543@holbertonschool.com>
2019-01-04 06:14:58 +0000

    Class Square with getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Class Square: contains optional size, area
+"""
+
+
+class Square:
+    """Create an square with private instance attribute: size"""
+    def __init__(self, size=0):
+        """ initializes a square with attributes """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ retrieves the size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the size with error handling"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """ instantiation with area """
+    def area(self):
+        return self.__size ** 2
