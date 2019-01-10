e6d9c0d312e1996edaa7db8e247a025115d401e1
truong21 <543@holbertonschool.com>
2019-01-10 05:33:55 +0000

    class rectangle from 2-rectangle.py with added __str__ method

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+
+Module Containing class Rectangle
+
+"""
+
+
+class Rectangle:
+    """ Rectangle Class with width and height attribute """
+    def __init__(self, width=0, height=0):
+        """ initializes a rectangle with width and height """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ retrieves width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ sets the width with error handling """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ retrieves height """
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        """ sets the width with error handling """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ public instantiation of area """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ public instantiation of perimeter """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """ string representation of a rectangle """
+        s = ""
+        if self.__height == 0 or self.__width == 0:
+            return s
+        for x in range(self.__height):
+            for y in range(self.__width):
+                s += "#"
+            if x != self.__height:
+                s += "\n"
+        return s
