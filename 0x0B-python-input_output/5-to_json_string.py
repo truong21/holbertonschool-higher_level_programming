d416b33215e61bfb87fe98a32e2fb6ebb004197f
truong21 <543@holbertonschool.com>
2019-01-18 00:44:01 +0000

    returns the JSON rep of an obj str

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing to_json_string function
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object(string) """
+    return json.dumps(my_obj)
