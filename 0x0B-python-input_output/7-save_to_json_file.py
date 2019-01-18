1649082178b3474fdc650eb9b39b4ca5b68a3296
truong21 <543@holbertonschool.com>
2019-01-18 01:41:14 +0000

    remove variable lc

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -7,6 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ writes an Object string to a text file using JSON rep """
-    lc = 0
     with open(filename, mode='w') as f:
         f.write(json.dumps(my_obj))
