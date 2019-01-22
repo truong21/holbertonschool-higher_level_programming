f6b268beb5ced6b79cdcdc16b02cd6b0cb3311b6
truong21 <543@holbertonschool.com>
2019-01-22 20:24:23 +0000

    switch from checking dir to type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,6 +7,6 @@ Module containing is_same_class function"
 def is_same_class(obj, a_class):
     """ function that returns True if the object is exactly an
         instance of the specified class ; otherwise False """
-    if dir(obj) == dir(a_class):
+    if type(obj) is a_class:
         return True
     return False
