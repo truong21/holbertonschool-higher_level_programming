0b0bdb7806c241378110fabc89d3e64cf55d1def
truong21 <543@holbertonschool.com>
2019-01-22 20:46:57 +0000

    pep8 fixes

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,6 @@ Module containing is_kind_of_class function"
 
 
 def is_kind_of_class(obj, a_class):
-    """ function that returns True if the object is exactly an instance of a 
+    """ function that returns True if the object is exactly an instance of a
         class that inherited from, the specific class ; otherwise False """
     return isinstance(obj, a_class)
