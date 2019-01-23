62373ecfa28b5ae66fbf827ca75e2278a0e06251
truong21 <543@holbertonschool.com>
2019-01-23 01:12:12 +0000

    remove class BaseGeometry code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 """
-Class BaseGeometry
-"""
-
-
-class BaseGeometry:
-    """ class BaseGeometry """
-    def area(self):
-        """checks for area """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-"""
 Class Rectangle(inherits from BaseGeometry
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
