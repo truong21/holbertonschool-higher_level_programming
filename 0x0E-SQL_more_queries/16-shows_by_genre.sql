d6efbc627241d5e75a7fba7424cc43cf120e16a5
truong21 <543@holbertonschool.com>
2019-03-06 22:14:28 +0000

    fix ordering

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_genres.name;
