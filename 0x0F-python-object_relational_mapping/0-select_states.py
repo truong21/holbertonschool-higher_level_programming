ddfa325fd0f6745f384fb15f6ef14dda02604e08
truong21 <543@holbertonschool.com>
2019-03-29 00:46:07 +0000

    pep8 style fixes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", user=username, passwd=pwd, db=db_name)
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=pwd,
+                         db=db_name)
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states")
     selec_data = cur.fetchall()
