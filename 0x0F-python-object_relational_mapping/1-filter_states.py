f4796d3f7c7e1d1304b83de59b904d4f2df37cd3
truong21 <543@holbertonschool.com>
2019-03-29 01:36:07 +0000

    pep8 style fixes

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", user=username, passwd=pwd, db=db_name)
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=pwd, db=db_name)
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states WHERE name like 'N%'")
     selec_data = cur.fetchall()
