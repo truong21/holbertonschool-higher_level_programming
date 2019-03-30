36988c2a6d84621ff0f1c9646ba0b8645c55fa38
truong21 <543@holbertonschool.com>
2019-03-30 01:50:28 +0000

    prevent SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module listing all states starting name matches from the database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=pwd, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY states.name LIKE %s;",
+                (name,))
+    selec_data = cur.fetchall()
+    for item in selec_data:
+        print(item)
+    db.close()
