643e09bfeb6c3209cfe3e338c7209b0e8b36a56f
truong21 <543@holbertonschool.com>
2019-03-31 09:28:46 +0000

    changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine_str = "mysql://{}:{}@localhost:3306/{}".format(username,
+                                                          passwd, db)
+    engine = create_engine(engine_str)
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).filter(State.id == 2):
+        instance.name = "New Mexico"
+    session.commit()
