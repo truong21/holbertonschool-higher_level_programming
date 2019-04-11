b034ea8887d88a9171ff3a36ebe664d7000c4942
truong21 <543@holbertonschool.com>
2019-04-11 23:05:44 +0000

    added variable for URL'

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to that URL, and displays the size of the body of the response
-curl -sd -X "POST" "subject=I will always be here for PLD&email=hr@holbertonschool.com"
+curl -X "POST" "$1" -sd "subject=I will always be here for PLD&email=hr@holbertonschool.com"
