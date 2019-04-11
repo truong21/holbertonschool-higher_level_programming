7a1ac810c2d313aeeecc09642c045b6c8d192223
truong21 <543@holbertonschool.com>
2019-04-11 05:25:27 +0000

    sends a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# takes in a URL, sends a POST request to that URL, and displays the size of the body of the response
+curl -sd -X "POST" "subject=I will always be here for PLD&email=hr@holbertonschool.com"
