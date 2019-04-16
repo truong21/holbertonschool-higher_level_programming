1bef4e87aec677dfb998c5147589920a6f1d1bc7
truong21 <543@holbertonschool.com>
2019-04-16 02:55:19 +0000

    pep8 fixes

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     values = {'email': email}
-    r = requests.post(url, data = values)
+    r = requests.post(url, data=values)
     print(r.text)
