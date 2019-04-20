5143e9999aa12ddb665d1394917b6d0278c6465f
truong21 <543@holbertonschool.com>
2019-04-20 05:47:00 +0000

    refactor peak algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """
-Module containing find_peak
+Module containing find_peak in a list of unsorted integers
 """
 
 
 def find_peak(list_of_integers):
     """ finds a peak in a list of unsorted integers """
-    if type(list_of_integers) is not list:
+    if not list_of_integers:
         return None
-    if len(list_of_integers) < 1:
-        return None
-    length = len(list_of_integers)
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
     peak = list_of_integers[0]
-    for num in list_of_integers:
-        if length == 1:
-            return peak
-        if num < peak:
-            return peak
-        if num > peak:
-            peak == num
+    for x in range(len(list_of_integers) - 1):
+        if list_of_integers[x + 1] > list_of_integers[x]:
+            peak = list_of_integers[x + 1]
+        elif list_of_integers[x] > list_of_integers[x + 1]:
+            peak = list_of_integers[x]
     return peak
