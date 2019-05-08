c431f5cf8c872ab31cbed7b6182cb0869317ec8b
truong21 <543@holbertonschool.com>
2019-05-08 16:57:54 +0000

    remove test prints

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,5 @@ const list = process.argv.slice(2);
 if (list.length === 0 || list.length === 1) {
   console.log('0');
 } else {
-  console.log(list);
-  console.log(list.sort(function (a, b) { return a - b; }));
   console.log(list.sort(function (a, b) { return a - b; }).reverse()[1]);
 }
