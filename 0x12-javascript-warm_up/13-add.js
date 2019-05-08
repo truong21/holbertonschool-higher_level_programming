3e296d232934e62036ca489d4adcf342b6c65556
truong21 <543@holbertonschool.com>
2019-05-08 05:00:58 +0000

    semistandard fixes

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function add (a, b) {
-    if (isNaN(a) || isNaN(b)) {
-        return ('NaN');
-    } else {
-        return (Number(a) + Number(b));
-    }
+  if (isNaN(a) || isNaN(b)) {
+    return ('NaN');
+  } else {
+    return (Number(a) + Number(b));
+  }
 }
 
 exports.add = add;
