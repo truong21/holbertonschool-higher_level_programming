ada50dec7b8b2ab1289895cf184ec074d80f88da
truong21 <543@holbertonschool.com>
2019-05-15 00:04:12 +0000

    made square inherit from square_rec

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square_Rec = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Square_Rec {
   constructor (size) {
     super(size, size);
   }
